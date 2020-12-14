2bbb6ee7112c30733e5a29aa95cd64a50fcf94fc
christinapassafaro <christinapassafaro@gmail.com>
2020-12-14 15:54:19 -0500

    u

--- index.js
+++ index.js
@@ -20,11 +20,24 @@ const commentController = require("./controllers/commentController")
 //MIDDLEWARE SETUP
 app.use(bodyParser.json());
 app.use(express.json());
-app.use(cors({
-  origin: ['http://localhost:3000', 'https://hardcore-jackson-68ff7b.netlify.app'],
-  methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
-  credentials: true
-}));
+// app.use(cors({
+//   origin: ['http://localhost:3000', 'https://hardcore-jackson-68ff7b.netlify.app'],
+//   methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
+//   credentials: true
+// }));
+
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  if ('OPTIONS' == req.method) {
+       res.send(200);
+   } else {
+       next();
+   }
+  });
 
 
 
