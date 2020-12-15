548eaf5fae8770cba882cd185607a0c48c434e58
christinapassafaro <christinapassafaro@gmail.com>
2020-12-15 15:22:58 -0500

    updates

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
-const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 const passport = require("passport");
-const passportlocal = require("passport-local").Strategy;
 const cookieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
 const session = require("express-session");
 const bodyParser = require("body-parser");
-const { deserializeUser } = require("passport");
 const User = require("./models/User");
 const app = express();
-const LocalStrategy = require("passport-local").Strategy;
+const jwt = require('jwt-simple');
 const userController = require("./controllers/userController");
-const notesController = require("./controllers/notesController")
-const riverController = require("./controllers/riversController")
-const commentController = require("./controllers/commentController")
+const notesController = require("./controllers/notesController");
+const riverController = require("./controllers/riversController");
+const commentController = require("./controllers/commentController");
+let secret = "asdof97h07y4t3";
 
 
 //MIDDLEWARE SETUP
+app.use(cors());
+app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
-app.use(express.json());
-app.use(cors({
-  origin: ['http://localhost:3000', 'https://hardcore-jackson-68ff7b.netlify.app'],
-  methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
-  credentials: true
-}));
+
+//middleware function
+function isAuthenticated(req, res, next) {
+  console.log(req)
+  if(req.headers.authorization){
+      try{
+          console.log("User has token");
+          let token = req.headers.authorization.split("Bearer ")[1];
+          console.log(token);
+        let accessToken = jwt.decode(token, config.jwtSecret);
+        return next();
+      }catch(err){
+          console.log(err);
+          console.log("There is something wrong with your token");
+          res.sendStatus(401)
+      }
+  }else{
+      res.sendStatus(401)
+      // console.log('error')
+  }
+}
+
 
 
 
@@ -36,10 +52,10 @@ app.use(
     saveUninitialized: true
   })
 );
-app.use(cookieParser("this will be our secret code"));
-app.use(passport.initialize());
-app.use(passport.session());
-require("./controllers/auth")(passport);
+// app.use(cookieParser("this will be our secret code"));
+// app.use(passport.initialize());
+// app.use(passport.session());
+// require("./controllers/auth")(passport);
 
 // ROUTES ALL
 
@@ -77,115 +93,137 @@ app.put('/favorites/:name', userController.edit)
 //route for deleting an element from the favorties array
 app.put('/favorites/:name/:fav', userController.deleteFavortie)
 
-
-// app.post('/register', async (req, res, next) => {
-// 	// console.log('what is going on', req.body);
-// 	const desiredUsername = req.body.username
-// 	const desiredPassword = req.body.password
-// 	const userWithThisUsername = await User.findOne({
-// 		username: desiredUsername
-// 	})
-// 	console.log(userWithThisUsername);
-// 	if(userWithThisUsername) {
-// 		req.session.message = `Username ${desiredUsername} already taken.`
-// 	}
-// 	else {
-// 		const createdUser = await User.create({
-// 			username: desiredUsername,
-// 			password: desiredPassword
-// 		})
-// 		req.session.loggedIn = true
-// 		req.session.userId = createdUser._id
-// 		req.session.username = createdUser.username
-// 		req.session.message = "Thank you for signing up, " + createdUser.username + "."
-// 		console.log('successful registration');
-// 	}
-// })
-
-// // LOGIN
-// app.post('/login', async (req, res, next) => {
-//   console.log('we are logging in', req.body)
-//   const user = await User.findOne({ username: req.body.username })
-//   if(!user) {
-//     req.session.message = "Invalid username or password."  
-//   }
-//   else {
-//     if(user.password == req.body.password) {
-//       console.log('did we make it in here')
-//       console.log('we have session', req.session)
-//       req.session.loggedIn = true
-//       req.session.userId = user._id
-//       req.session.username = user.username
-//       req.session.message = "Welcome back, " + user.username + "."
-//       console.log('after', req.session)
-//       res.send(req.session)
-//     }
-//     else {
-//       req.session.message = "Invalid username or password."
-//     }
-//   }
-// })
-
-
-// // GET USER
-// app.get('/getUser', (req, res) => res.send(req.session))
-
-
-
-
-
-
 /////////////******************************************* */
 //function to login users
-app.post("/login", (req, res, next) => {
-  // use local strategy we defined
-    User.find({username:req.body.username}).then(userFind =>{ 
-      console.log(userFind)
-        passport.authenticate("local", (err, user) => {
-            if (err) console.log(err);
-            if (userFind.length != 1) {
-              res.send("Username is incorrect");
-            } else if (user == false) {
-              res.send("Password is incorrect");
-            } else {
-              req.login(user, (err) => {
-                if (err) throw err;
-                res.send("Successfully Authenticated");
-              });
-            }
-          })(req, res, next);
-        })
-});
+// app.post("/login", (req, res, next) => {
+//   // use local strategy we defined
+//     User.find({username:req.body.username}).then(userFind =>{ 
+//       console.log(userFind)
+//         passport.authenticate("local", (err, user) => {
+//             if (err) console.log(err);
+//             if (userFind.length != 1) {
+//               res.send("Username is incorrect");
+//             } else if (user == false) {
+//               res.send("Password is incorrect");
+//             } else {
+//               req.login(user, (err) => {
+//                 if (err) throw err;
+//                 res.send("Successfully Authenticated");
+//               });
+//             }
+//           })(req, res, next);
+//         })
+// });
 
 // registers users
-app.post("/register", (req, res) => {
-  User.findOne({ username: req.body.username }, async (err, doc) => {
-    if (err) throw err;
-    if (doc) res.send("User Already Exists");
-    if (!doc) {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10);
-      const newUser = new User({
-        username: req.body.username,
-        password: hashedPassword,
-      });
-      await newUser.save();
-      res.send("User Created");
-    }
-  });
-});
+// app.post("/register", (req, res) => {
+//   User.findOne({ username: req.body.username }, async (err, doc) => {
+//     if (err) throw err;
+//     if (doc) res.send("User Already Exists");
+//     if (!doc) {
+//       const hashedPassword = await bcrypt.hash(req.body.password, 10);
+//       const newUser = new User({
+//         username: req.body.username,
+//         password: hashedPassword,
+//       });
+//       await newUser.save();
+//       res.send("User Created");
+//     }
+//   });
+// });
 
 // req.user stores the user
 // req object will not be a user object containing session data
 // accessible throughout whole app
-app.get('/getUser', (req, res) => {
-  console.log(req)
-  console.log('---------------------')
-  console.log(req.sessions)
-  res.send(req.user)
-});
+// app.get('/getUser', (req, res) => {
+//   console.log(req)
+//   console.log('---------------------')
+//   console.log(req.sessions)
+//   res.send(req.user)
+// });
 
 /***************************************************** */
 
+
+app.post('/signup', (req, res) => {
+  if (req.body.username && req.body.password) {
+    let newUser = {
+      username: req.body.username,
+      password: req.body.password
+    }
+    console.log(newUser);
+    User.findOne({ username: req.body.username })
+      .then((user) => {
+          console.log("29: ",user);
+        if (!user) {
+          console.log("creating new user")
+          User.create(newUser)
+            .then(user => {
+              if (user) {
+                var payload = {
+                  id: newUser.id,
+                  userIsLoggedIn:true
+                }
+                var token = jwt.encode(payload, secret)
+                res.json({
+                  token: token
+                })
+              } else {
+                res.sendStatus(401)
+              }
+            })
+        } else {
+          res.sendStatus(401)
+        }
+      })
+  } else {
+    res.sendStatus(401)
+  }
+})
+
+app.post('/login', (req, res) => {
+  if (req.body.username && req.body.password) {
+    User.findOne({ username: req.body.username}).then(user => {
+      if (user) {
+          console.log(user.id);
+        if (user.password === req.body.password) {
+          var payload = {
+            id: user.id,
+            userIsLoggedIn:true,
+            access:"basic-user"
+          }
+          var token = jwt.encode(payload, secret)
+          res.json({
+            token: token,
+            loggedIn:true,
+            username:req.body.username
+          })
+        } else {
+          res.sendStatus(401)
+        }
+      } else {
+        res.sendStatus(401)
+      }
+    })
+  } else {
+    res.sendStatus(401)
+  }
+})
+
+app.get('/logout', (req, res) => {
+  res.send({ hello: 'World' })
+})
+
+app.get('/private', isAuthenticated,
+(req, res) => {
+  res.json({loggedIn:true})
+}
+);
+app.get('/user',
+//connectEnsureLogin.ensureLoggedIn(),
+
+(req, res) => res.send({user: req.user})
+);
 
 
 app.set("port", process.env.PORT || 8080);
@@ -196,3 +234,12 @@ app.listen(app.get("port"), () => {
 
 
 //hello
+
+
+// app.use(bodyParser.json());
+// app.use(express.json());
+// app.use(cors({
+//   origin: ['http://localhost:3000', 'https://hardcore-jackson-68ff7b.netlify.app'],
+//   methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
+//   credentials: true
+// }));
