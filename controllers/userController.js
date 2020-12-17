fae19451bccdcab44739bfd3f9326523718b641c
christinapassafaro <christinapassafaro@gmail.com>
2020-12-17 12:49:14 -0500

    updates to routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,30 +19,10 @@ module.exports = {
         User.findOneAndUpdate(
             {username: req.params.name},
             {$pull: {'favorites': {name: req.params.river}}}, { safe: true, upsert: true },
-            // {multi: true} //sets to remove multiple element
-            // {new:true})
             function(err, node) {
                 if (err) { return handleError(res, err); }
                 return res.status(200).json(node.favorites.name);
             })
-
-    //         { $pull: { results: { $elemMatch: { score: 8 , item: "B" } } } }, // item(s) to match from array you want to pull/remove
-    // { multi: true }
-            // .then(user => {
-            //     res.send(user) 
-                // console.log(res)
-                // let index = ''
-                // let fav = user.favorites
-                // for(let i =0; i< fav.length; i++){
-                //     if(fav[i] == req.body.fav){
-                //         let index = i
-                //         return index
-                //     }
-                //     return index
-                // }
-                // console.log(index)
-            // })
-        // )
     }
 }
 
